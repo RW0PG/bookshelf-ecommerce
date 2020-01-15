9e278d0362292f3ceacb0740bbb109fcf77f0f6b
rw0pg <karolmaczuga@gmail.com>
2020-01-15 20:09:35 +0100

    The first version of repo

---
+++ src/main/java/pl/rw0pg/kijo22/books/App.java
@@ -0,0 +1,11 @@
+package pl.rw0pg.kijo22.books;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+//        SpringApplication.run(Appnit.class, args);
+    }
+}
